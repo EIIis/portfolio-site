61965b99f0e33416fc87b5a60c532b122bf4b6ea
Ellis Alcantara <ellisalcantara@gmail.com>
2020-11-16 01:43:24 -0600

    Finished off the homepage and the 'carousel'

--- README.md
+++ README.md
@@ -10,3 +10,5 @@ Used JavaScript, CSS, HTML, and the JS framework - React
 ### Created by using "Create React"
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+#### Project includes elements from React, including react-bootstrap and react-spring
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import  BigHome from '../components/BigHome'
+import Carousel from '../components/Carousel';
 
 function Homepage(props){
 
     return(
+        <div>
         <BigHome title={props.title} subtitle={props.subtitle} text={props.text} />
-
+        <Carousel />
+        </div>
     );
 }
 export default Homepage;
