b43c551dfc907d70a929eb7a185e5d925ae78962
Ellis Alcantara <ellisalcantara@gmail.com>
2021-10-01 20:57:35 -0500

    Updated portfolio

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -7,6 +7,9 @@ import wavImage from "../assets/wav.svg" // Change/Add more when you have more p
 import twitter from "../assets/tweet.svg"
 import caviarCoin from "../assets/logo.svg"
 import portHome from "../assets/homepage.svg"
+import github from "../assets/GitHub-Mark.svg"
+import LingoMate from "../assets/LingoMate.svg"
+import blackjack from "../assets/blackjack.png"
 
 class Carousel extends React.Component{
     constructor(props){
@@ -15,56 +18,75 @@ class Carousel extends React.Component{
             items: [
             { // Add another bracket, while adding +1 to the latest id to add another project
                 id: 0,
-                title: "Blog Site",
-                subtitle: "A Twitter bot which replies to users",
-                imgSrc: twitter,
-                link: "http://github.com/EIIis/blog-site",
-                selected: false
-            },
-            {
-                id: 1,
-                title: "Algorithm Visualization Site",
-                subtitle: "A Twitter bot which replies to users",
-                subtitle2: "SHESSH",
-                imgSrc: twitter,
-                link: "http://github.com/EIIis/algo-visualization",
-                selected: false
-            },
-            { 
-                id: 2,
                 title: "Portfoilio Site",
                 subtitle: "Portfolio site (site you're on) created with",
                 subtitle2: "React and JavaScript to display myself!",
                 imgSrc: portHome,
-                link: "http://github.com/EIIis/portfolio-site",
+                link: <a href="http://github.com/EIIis/portfolio-site" target="_blank" rel="noopener noreferrer">Link to site's source code</a>,
                 selected: false
             },
+            { 
+                id: 1,
+                title: "Caviar Coin",
+                subtitle: "Worked with a team to developed the",
+                subtitle2: "front-end of a website for 'Cavair Coin'",
+                imgSrc: caviarCoin,
+                link: <a href="https://bit.ly/CaviarCoin" target="_blank" rel="noopener noreferrer">Link to Caviar site</a>,
+                selected: false  
+            },
             {
-                id: 3,
+                id: 2,
                 title: "Sentiment Twitter Analysis ",
                 subtitle: "Analyzes a given user's tweers in order",
                 subtitle2: "to determine the sentimental value.",
                 imgSrc: twitter,
-                link: "http://github.com/EIIis/sentiment-analysis",
+                link: <a href="http://github.com/EIIis/sentiment-analysis" target="_blank" rel="noopener noreferrer">GitHub to source code</a>,
                 selected: false
             },
             { 
-                id: 4,
-                title: "Audio 2 Text",
+                id: 3,
+                title: "Audio To Text",
                 subtitle: "Uses a .wav file and returns a transcript",
                 imgSrc: wavImage,
-                link: "http://github.com/EIIis/audio-recog",
+                link: <a href="http://github.com/EIIis/audio-recog" target="_blank" rel="noopener noreferrer">GitHub to source code</a>,
                 selected: false
                 
             },
-            { 
+            {
+                id: 4,
+                title: "LingoMate",
+                subtitle: "A web app that allows users to match",
+                subtitle2: "with new people to learn language.",
+                imgSrc: LingoMate,
+                link: <a href="http://github.com/EIIis/LingoMate" target="_blank" rel="noopener noreferrer">GitHub to LingoMate</a>,
+                selected: false
+            },
+            {
                 id: 5,
-                title: "CaviarCoin",
-                subtitle: "Worked with a team to developed the",
-                subtitle2: "front-end of a website for 'Cavair Coin'",
-                imgSrc: caviarCoin,
-                link: "No live-site/Private Repo! Sorry :(",
-                selected: false  
+                title: "Blackjack",
+                subtitle: "A simple terminal based blackjack game",
+                subtitle2: "developed in Python.",
+                imgSrc: blackjack,
+                link: <a href="https://github.com/EIIis/Blackjack" target="_blank" rel="noopener noreferrer">GitHub to Blackjack</a>,
+                selected: false,
+            },
+            {
+                id: 6,
+                title: "Blog Site",
+                subtitle: "My blogsite written in TypeScript.",
+                subtitle2: "In Development, check Github! ",
+                imgSrc: github,
+                link: <a href="http://github.com/EIIis/blog-site" target="_blank" rel="noopener noreferrer">GitHub to Blogsite</a>,
+                selected: false
+            },
+            {
+                id: 7,
+                title: "Algorithm Visualization Site",
+                subtitle: "Algorithmn Visualizer site developed",
+                subtitle2: "in JavaScript. In-progress, no code",
+                imgSrc: github,
+                link: <a href="https://github.com/EIIis/algo-visualization" target="_blank" rel="noopener noreferrer">GitHub to source code</a>,
+                selected: false
             }
         ] 
         }
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import BigHome from '../components/BigHome';
 import Body from '../components/Body';
+import resume from '../assets/Ellis_Alcantara2023.pdf';
 
 function About(props) {
 
@@ -8,11 +9,31 @@ function About(props) {
         <div>
             <BigHome title={props.title} />
             <Body>
-            <p>Hi, my name is Ellis Alcantara! I'm a current student at the University of Alabama - Birmingham studying computer science and mathematics with a concentration in applied mathematics and scientific computation. </p>
-            <p>While I do love mathematics, specifically in optimization theory, I found software engineering super fun and I inspire to become a software engineering post-graduation. If you haven't checked out my projects on my homepage, you can checkout my <a href="https://github.com/EIIis"> Github here</a> which will show you all my projects. You can also check out my <a href="https://drive.google.com/file/d/1-1VtKJVsWgl909bM3tefrxuUrnwdll9x/view?usp=sharing"> resume here!</a> </p>
-            <p>I'm currently interning at HiveRnD which is a startup, from UAB since 2020. HiveRnD is developing a platform to connect physicians with unsolved problems to students and scientists willing to solve them. I'm currently working on the front-end of the site. </p>
-            <p>While academics and work-related things take up a majority of my time I do love to do thing outside of that. I like to play videogames, like Call of Duty, CS:GO, and Old School Runescape. I also enjoy keeping up with my favorite sport teams, such as the Atlanta Falcons, Atlanta Hawks, and Athletico Madrid!</p>
-            <p>If you want to talk, hit me up on <a href="https://www.linkedin.com/in/ellis-alcantara/">LinkedIn</a>, I'm always looking to connect with people!</p>
+            <p>Hi, my name is Ellis Alcantara Martinez! I'm a first-gen college student at the University of Alabama - Birmingham studying 
+                computer science and mathematics with a concentration in applied mathematics and scientific computation. </p>
+            
+            <p>I'm deeply passionate about diversity and inclusion in tech and can proudly say that I was picked as a finalist for 
+            <a href="https://www.code2040.org/fellows-program " target="_blank" rel="noopener noreferrer"> CODE2040 Fellows</a> program.
+            If you're an URM in tech, I highly recommend taking a look at CODE2040 and at ColorStack. Also feel free to reach out to me!
+            </p>
+
+            <p>While I do love mathematics, specifically optimization theory, I found software engineering super fun and I inspire to become 
+                a software engineering post-graduation. If you haven't checked out my projects on my homepage, you can checkout 
+                my <a href="https://github.com/EIIis " target="_blank" rel="noopener noreferrer"> Github here</a>, which will show you all my projects.
+                You can also download my <a href={resume} download> resume here</a>.</p> 
+
+            <p>During my summer 2021 break, I interned at a pre-launch SAAS startup called HiveRnD. The product we were developing was aplatform to connect 
+                physicians, who had unsolved problems, to students and scientists willing to solve them. I worked as a front-end engineer intern, where I worked with
+                the following tech-stack: JavaScript, TypeScript, Node.js, and Next.js. While I really enjoyed the feeling of working on a real product, I'm
+                looking for a company with {'>'}15 people (so feel free to hmu startups!) to work with for the summer of 2022! </p>
+
+            <p>While academics and work-related things take up a majority of my time I do love to do thing outside of that. I like to play videogames,
+                like Call of Duty, CS:GO, and Old School Runescape. I also enjoy keeping up with my favorite sport teams, such as the Atlanta Falcons,
+                Atlanta Hawks, and Athletico Madrid!</p>
+                
+            <p>If you want to talk, feel free to hit me up on <a href="https://www.linkedin.com/in/ellis-alcantara/" target="_blank" rel="noopener noreferrer">LinkedIn</a>,
+                I'm always looking to connect with people!</p>
+
             </Body>
         </div>
     );
